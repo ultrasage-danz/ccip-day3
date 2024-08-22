99013acde0844e9b755671cc790be38552723a8f
ultrasage-danz <olokodanielonline@gmail.com>
2024-08-22 15:23:28 +0100

    CCIP Day 3 Homework

---
+++ work.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+contract Empty {}
